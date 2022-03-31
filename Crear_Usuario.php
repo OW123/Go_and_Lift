8acb1177c87a0e72a3f035c0345a89292408a4c1
cesarww <98102923+cesarww@users.noreply.github.com>
2022-03-31 08:13:04 -0600

    Add files via upload

---
+++ Crear_Usuario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<form action="login.php" method="POST">
+   <input type="text" name="usuario" placeholder="ingrese correo">
+   <br><br>
+   <input type="password" name="clave" placeholder="ingrese contraseña">
+   <br><br>
+   <button type="sumbit">ENTRAR</button>
+   <script>
+        console.log(document.cookie);
+    </script>
+</body>
+</html>
